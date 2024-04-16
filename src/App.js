a5727701e40e2ebce9985447be082b9f63d49137
Sujithra <122441546+nynika@users.noreply.github.com>
2024-04-16 16:26:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Lab from './components/Lab/Lab';
 import Admission from './components/Admission/Admission';
 import Pharmacy from './components/Pharmacy/Pharmacy';
 import Login from './components/Login/Login'; 
+import Dashboard from './components/Dashboard/Dashboard'; 
 import './App.css';
 
 function App() {
@@ -21,26 +22,30 @@ function App() {
       <div className="App">
         <h1 align="center">Patient Tracking</h1>
         <nav>
-          <ul>
-            <li><Link to="/">OverallView</Link></li>
+        {/*   <ul>
+      
             {loggedInUser && ( 
               <>
+               <li><Link to="/overallView">OverallView</Link></li>
                 <li><Link to="/radiology">Radiology</Link></li>
                 <li><Link to="/lab">Lab</Link></li>
                 <li><Link to="/admission">Admission</Link></li>
                 <li><Link to="/pharmacy">Pharmacy</Link></li>
               </>
             )}
-          </ul>
+          </ul> */}
         </nav>
         <div className="content">
           <Routes>
             <Route exact path="/login" element={<Login onLogin={handleLogin} />} />
-            <Route exact path="/" element={<OverallView />} />
+            <Route exact path="/Dashboard" element={<Dashboard />} />
+            {loggedInUser === "overallView" && <Route exact path="/overallView" element={<OverallView />} />}
+
             {loggedInUser === "radiology" && <Route exact path="/radiology" element={<Radiology />} />}
-            {loggedInUser === "lab" && <Route exact path="/lab" element={<Lab />} />}
+            {loggedInUser === "lab" && <Route exact path="/lab" element={<Lab />} />} 
             {loggedInUser === "admission" && <Route exact path="/admission" element={<Admission />} />}
             {loggedInUser === "pharmacy" && <Route exact path="/pharmacy" element={<Pharmacy />} />}
+      
             {!loggedInUser && <Route path="*" element={<Navigate to="/login" />} />}
           </Routes>
         </div>
